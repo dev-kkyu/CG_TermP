3a45b2edb0c917d2364e54fe1c36f81751b9d8c8
devkkyu <104413535+devkkyu@users.noreply.github.com>
2022-12-06 02:11:33 +0900

    머지한거 오류해결

--- DreamCraft/DreamCraft/CWorld.cpp
+++ DreamCraft/DreamCraft/CWorld.cpp
@@ -1,8 +1,8 @@
-#include "CWorld.h"
+﻿#include "CWorld.h"
 
 CWorld::CWorld() : Player{ CPlayer{glm::vec3(0.f, 2.f, 0.f)} }, PlayerPos{ glm::vec3(0.f, 2.f, 0.f) },
 isUp{ false }, isDown{ false }, isLeft{ false }, isRight{ false }, isJump{ 0 }, personView{ 1 },
-planToCreateObj{ �⺻���� },
+planToCreateObj{ 기본흙 },
 first_VEL{ 25 }, MASS{ 10 }
 {
 	VELOCITY = first_VEL;
@@ -13,72 +13,72 @@ void CWorld::Keyboard(unsigned char key, int state)
 {
 	switch (state) {
 	case GLUT_DOWN:
-		switch (key) {					//�� ���������� ������
-			// ����
+		switch (key) {					//뭐 생성할지도 정해줌
+			// 무기
 		case '1':
-			Player.setWeapon(�Ǽ�);
+			Player.setWeapon(맨손);
 			break;
 		case '2':
-			Player.setWeapon(Į);
+			Player.setWeapon(칼);
 			break;
 		case '3':
-			Player.setWeapon(����);
+			Player.setWeapon(가위);
 			break;
 		case '4':
-			Player.setWeapon(���);
+			Player.setWeapon(곡괭이);
 			break;
 		case '9':
-			Player.setWeapon(�ְ�����);
+			Player.setWeapon(최강무기);
 			break;
 
-			// ����
+			// 블록
 		case 'u':
-			planToCreateObj = �⺻��;
+			planToCreateObj = 기본흙;
 			break;
 		case 'i':
-			planToCreateObj = �ܵ���;
+			planToCreateObj = 잔디흙;
 			break;
 		case 'o':
-			planToCreateObj = ��;
+			planToCreateObj = 벼;
 			break;
 		case 'j':
-			planToCreateObj = ����;
+			planToCreateObj = 양털;
 			break;
-		/*case 'k':
-			planToCreateObj = ;
-			break;
-		case 'l':
-			planToCreateObj = ;
-			break;*/
+			/*case 'k':
+				planToCreateObj = ;
+				break;
+			case 'l':
+				planToCreateObj = ;
+				break;*/
 		case 'm':
-			planToCreateObj = ����;
+			planToCreateObj = 나무;
 			break;
 		case ',':
-			planToCreateObj = �����ٱ�;
+			planToCreateObj = 나무줄기;
 			break;
 		case '.':
-			planToCreateObj = ������;
+			planToCreateObj = 나뭇잎;
 			break;
 
 
-			// ����
+			// 동물
 		case 'z':
-			planToCreateObj = ����;
+			planToCreateObj = 돼지;
 			break;
 		case 'x':
-			planToCreateObj = ��;
+			planToCreateObj = 소;
 			break;
 		case 'c':
-			planToCreateObj = ��;
+			planToCreateObj = 닭;
 			break;
 		case 'v':
-			planToCreateObj = ��;
+			planToCreateObj = 양;
 			break;
 		case 'b':
-			planToCreateObj = ���ʾ�;
+			planToCreateObj = 무너양;
 			break;
 
-			//����
+			//시점
 		case 'f':
 			personView = 1;
 			break;
@@ -86,7 +86,7 @@ void CWorld::Keyboard(unsigned char key, int state)
 			personView = 3;
 			break;
 
-			// �̵�
+			// 이동
 		case 'w':
 		case 'W':
 			isUp = true;
@@ -103,14 +103,12 @@ void CWorld::Keyboard(unsigned char key, int state)
 		case 'D':
 			isRight = true;
 			break;
-		//case 'j':
-		//case 'J':
 		case ' ':
 			if (isJump != 2) {
 				++isJump;
 			}
 			break;
-		case 27:		//Esc��ư
+		case 27:		//Esc버튼
 			glutLeaveMainLoop();
 			break;
 		default:
@@ -214,8 +212,8 @@ void CWorld::Mouse(int button, int state)
 void CWorld::Camera()
 {
 	if (1 == personView) {
-		// ī�޶� ��ȯ
-		glm::vec3 DirectionPos{ 0, 0, -5.f };				//ī�޶� ���� ����
+		// ī ޶    ȯ
+		glm::vec3 DirectionPos{ 0, 0, -5.f };				//ī ޶           
 		glm::mat4 cameraDirRot = glm::rotate(glm::mat4(1.f), glm::radians(-MouseAngle.first), glm::vec3(0.f, 1.f, 0.f));
 		cameraDirRot = glm::rotate(cameraDirRot, glm::radians(-MouseAngle.second), glm::vec3(1.f, 0.f, 0.f));
 		DirectionPos = cameraDirRot * glm::vec4(DirectionPos, 1.f);
@@ -223,17 +221,17 @@ void CWorld::Camera()
 		glm::vec3 cPos = glm::vec3(0, 0, -0.25);
 		cPos = glm::rotate(glm::mat4(1.f), glm::radians(-MouseAngle.first), glm::vec3(0.f, 1.f, 0.f)) * glm::vec4(cPos, 1.f);
 
-		cameraPos = glm::vec3(PlayerPos.x + cPos.x, PlayerPos.y - 0.4f, PlayerPos.z + cPos.z); //--- ī�޶� ��ġ (��� ������)
-		cameraDirection = PlayerPos + DirectionPos; //--- ī�޶� �ٶ󺸴� ���� (��𺼰��� �ϸ�ɵ�)
-		glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f); //--- ī�޶� ���� ����->������(���⸸) (�����ϸ� ȭ�� ���ϰŲٷκ���)
+		cameraPos = glm::vec3(PlayerPos.x + cPos.x, PlayerPos.y - 0.4f, PlayerPos.z + cPos.z); //--- ī ޶    ġ (          )
+		cameraDirection = PlayerPos + DirectionPos; //--- ī ޶   ٶ󺸴       (  𺼰     ϸ ɵ )
+		glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f); //--- ī ޶           ->      (   ⸸) (     ϸ  ȭ      ϰŲٷκ   )
 
 		glm::mat4 view = glm::lookAt(cameraPos, cameraDirection, cameraUp);// *cameraRevolution;
 
-		GLuint viewLocation = glGetUniformLocation(shaderID, "viewTransform"); //--- ���� ��ȯ ����
+		GLuint viewLocation = glGetUniformLocation(shaderID, "viewTransform"); //---        ȯ     
 		glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
 
-		// fragment ���̴��� �Ѿ�� ī�޶� ��ǥ(��������)
-		GLuint viewPosLocation = glGetUniformLocation(shaderID, "viewPos");		//--- viewPos �� ����: ī�޶� ��ġ
+		// fragment    ̴     Ѿ   ī ޶    ǥ(        )
+		GLuint viewPosLocation = glGetUniformLocation(shaderID, "viewPos");		//--- viewPos        : ī ޶    ġ
 		glUniform3f(viewPosLocation, cameraPos.x, cameraPos.y, cameraPos.z);
 
 
@@ -241,8 +239,8 @@ void CWorld::Camera()
 
 	}
 	else if (3 == personView) {
-		// ī�޶� ��ȯ
-		glm::vec3 DirectionPos{ 0, 0, -5.f };				//ī�޶� ���� ����
+		// ī ޶    ȯ
+		glm::vec3 DirectionPos{ 0, 0, -5.f };				//ī ޶           
 		glm::mat4 cameraDirRot = glm::rotate(glm::mat4(1.f), glm::radians(-MouseAngle.first), glm::vec3(0.f, 1.f, 0.f));
 		cameraDirRot = glm::rotate(cameraDirRot, glm::radians(-MouseAngle.second), glm::vec3(1.f, 0.f, 0.f));
 		DirectionPos = cameraDirRot * glm::vec4(DirectionPos, 1.f);
@@ -250,17 +248,17 @@ void CWorld::Camera()
 		glm::vec3 cPos = glm::vec3(0, 0, -0.25);
 		cPos = glm::rotate(glm::mat4(1.f), glm::radians(-MouseAngle.first), glm::vec3(0.f, 1.f, 0.f)) * glm::vec4(cPos, 1.f);
 
-		cameraPos = glm::vec3(PlayerPos.x + cPos.x, PlayerPos.y - 0.4f, PlayerPos.z + cPos.z); //--- ī�޶� ��ġ (��� ������)
-		cameraDirection = PlayerPos + DirectionPos; //--- ī�޶� �ٶ󺸴� ���� (��𺼰��� �ϸ�ɵ�)
-		glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f); //--- ī�޶� ���� ����->������(���⸸) (�����ϸ� ȭ�� ���ϰŲٷκ���)
+		cameraPos = glm::vec3(PlayerPos.x + cPos.x, PlayerPos.y - 0.4f, PlayerPos.z + cPos.z); //--- ī ޶    ġ (          )
+		cameraDirection = PlayerPos + DirectionPos; //--- ī ޶   ٶ󺸴       (  𺼰     ϸ ɵ )
+		glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f); //--- ī ޶           ->      (   ⸸) (     ϸ  ȭ      ϰŲٷκ   )
 
 		glm::mat4 view = glm::translate(glm::mat4(1.f), glm::vec3(0, -1, -5)) * glm::lookAt(cameraPos, cameraDirection, cameraUp);// *cameraRevolution;
 
-		GLuint viewLocation = glGetUniformLocation(shaderID, "viewTransform"); //--- ���� ��ȯ ����
+		GLuint viewLocation = glGetUniformLocation(shaderID, "viewTransform"); //---        ȯ     
 		glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
 
-		// fragment ���̴��� �Ѿ�� ī�޶� ��ǥ(��������)
-		GLuint viewPosLocation = glGetUniformLocation(shaderID, "viewPos");		//--- viewPos �� ����: ī�޶� ��ġ
+		// fragment    ̴     Ѿ   ī ޶    ǥ(        )
+		GLuint viewPosLocation = glGetUniformLocation(shaderID, "viewPos");		//--- viewPos        : ī ޶    ġ
 		glUniform3f(viewPosLocation, cameraPos.x, cameraPos.y, cameraPos.z);
 	}
 }
@@ -331,46 +329,46 @@ void CWorld::addNewObject(int ObjectType)
 			tempPos.y = ceil(tempPos.y);
 			tempPos.z = round(tempPos.z);
 			switch (ObjectType) {
-			case �⺻��:
+			case 기본흙:
 				Objects.insert(new CBase(tempPos));
 				break;
-			case �ܵ���:
+			case 잔디흙:
 				Objects.insert(new CGrass(tempPos));
 				break;
-			//case ��:
-			//	Objects.insert(new CBase(tempPos));
-			//	break;
-			//case ����:
-			//	Objects.insert(new CBase(tempPos));
-			//	break;
+				//case 벼:
+				//	Objects.insert(new CBase(tempPos));
+				//	break;
+				//case 양털:
+				//	Objects.insert(new CBase(tempPos));
+				//	break;
 
-			case ����:
-				 MakeTree(tempPos);
+			case 나무:
+				MakeTree(tempPos);
 				break;
-			case �����ٱ�:
+			case 나무줄기:
 				Objects.insert(new CTreeTrunk(tempPos));
 				break;
-			case ������:
+			case 나뭇잎:
 				Objects.insert(new CLeaves(tempPos));
 				break;
 
 
-			case ����:
+			case 돼지:
 				Objects.insert(new CPig(tempPos));
 				break;
-			case ��:
+			case 소:
 				Objects.insert(new CCow(tempPos));
 				break;
-			case ��:
+			case 닭:
 				Objects.insert(new CChicken(tempPos));
 				break;
-			case ��:
+			case 양:
 				Objects.insert(new CSheep(tempPos));
 				break;
-			case ���ʾ�:
+			case 무너양:
 				Objects.insert(new CSheepNOTUL(tempPos));
 				break;
-			
+
 			}
 			break;
 		}
@@ -381,11 +379,11 @@ void CWorld::addNewObject(int ObjectType)
 
 void CWorld::Initialize()
 {
-	// �ٴ�
+	// 바닥
 	for (int i = -25; i <= 24; ++i)
 		for (int j = -25; j <= 24; ++j)
 			Objects.insert(new CGrass{ glm::vec3{i, 0, j} });
-	// �ٴ� �Ʒ�
+	// 바닥 아래
 	for (int k = -1; k > -5; --k)
 		for (int i = -25; i <= 24; ++i)
 			for (int j = -25; j <= 24; ++j)
@@ -427,7 +425,7 @@ void CWorld::Update()
 		time = 0;
 	}
 
-	
+
 
 	if (isUp) {
 		PlayerPos.x += glm::sin(glm::radians(MouseAngle.first)) * 0.075f;
@@ -495,7 +493,7 @@ void CWorld::MakeTree(glm::vec3 position)
 
 	for (int i = -2; i < 3; ++i) {
 		for (int j = -2; j < 3; ++j) {
-			if (!(i == -2 && j == -2)&& !(i == -2 && j == 2) && !(i == 2 && j == 2) && !(i == 2 && j == -2))
+			if (!(i == -2 && j == -2) && !(i == -2 && j == 2) && !(i == 2 && j == 2) && !(i == 2 && j == -2))
 				Objects.insert(new CLeaves{ glm::vec3(x + i,y + 3,z + j) });
 		}
 	}
@@ -504,14 +502,14 @@ void CWorld::MakeTree(glm::vec3 position)
 	for (int i = -1; i < 2; ++i) {
 		for (int j = -1; j < 2; ++j) {
 			if (!(i == 1 && j == 1))
-			Objects.insert(new CLeaves{ glm::vec3(x + i,y + 4,z + j) });
+				Objects.insert(new CLeaves{ glm::vec3(x + i,y + 4,z + j) });
 		}
 	}
 
 	Objects.insert(new CLeaves{ glm::vec3(x ,y + 5,z) });
 	Objects.insert(new CLeaves{ glm::vec3(x ,y + 5,z + 1) });
 
-	Objects.insert(new CLeaves{ glm::vec3(x ,y + 6,z ) });
+	Objects.insert(new CLeaves{ glm::vec3(x ,y + 6,z) });
 
 
 }
