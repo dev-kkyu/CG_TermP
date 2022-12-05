d897ec848f2fa9e0efb7eec892165e5e3d3c4697
박자은 <ddoavoca@gmail.com>
2022-12-05 21:38:51 +0900

    28

---
+++ DreamCraft/DreamCraft/CTree.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "CBlock.h"
+
+class CTree : public CBlock
+{
+private:
+
+public:
+	CTree(glm::vec3 Position);
+	virtual ~CTree();			//소멸자도 버츄얼 해주는게 좋음
+
+	virtual void Initialize() override;		//생성될 때 할 일
+	virtual void Update() override;			//타이머에서 할 일
+	virtual void FixedUpdate() override;		//충돌처리 등
+	virtual void Render() override;			//드로우
+	virtual void Release() override;			//소멸될 때 할 일
+};
+
